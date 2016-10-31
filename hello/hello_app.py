a598b507bad9036179004159963fff3e700a817a
sempoisari <sarina.kptm@yahoo.com>
2016-11-01 06:17:29 +0800

    add comment
    edit code

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def hello():
     return "Hello World, I love continuous delivery a lot and I enjoyed learn SCM!..Yes best"
 
-if __name__ == "__main__":
+if __name__ == "__main__//":
     app.run()
