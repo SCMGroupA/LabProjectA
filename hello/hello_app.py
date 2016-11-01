3aa031de661271fa29e5da3ea327b41351063f42
suhaimilkpp <suhaimi7216@yahoo.com>
2016-11-01 15:44:59 +0800

    I ganti We
    I ganti We

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, I love continuous delivery a lot and I enjoyed learn SCM!..Yes best"
+    return "Hello World, we love continuous delivery a lot and we enjoyed learn SCM!..Yes best"
 
 if __name__ == "__main__":
     app.run()
