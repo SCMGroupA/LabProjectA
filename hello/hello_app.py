1c72ccafe923dc62a2303fa3e13a63c24f930faa
fara1601 <fara_1601@yahoo.com>
2016-12-07 21:54:02 +0800

    try to insert image

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><b><center>Mini Project SCM <p>Marziah, Fara, Sarina, Suhaimi</center></b></html>"
+    return "<html><b><center>Mini Project SCM <p>Marziah, Fara, Sarina, Suhaimi</center></b>
+            <p><img src="img/robot.jpg" width="600" height="200" />
+	</html>"
 
 
 if __name__ == "__main__":
