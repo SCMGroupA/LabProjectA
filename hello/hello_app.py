e6793a5fdd24af40e093c591ca5b82a2e50945d7
fara1601 <fara_1601@yahoo.com>
2016-12-07 16:11:21 +0800

    updated

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,17 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return """<html>
-<head></head>
-<body>
-	<p><u>Mini Project Software Configuration Management MCS1044</u></p>
-	<p><i>Group Members:</i></p>
-		<p>Marziah Abdul Wahab KCS15001 </p>
-		<p>Nor Faradilla Mohamed Idris KCS15002 </p>
-		<p>Sarina Abu Bakar KCS15004 </p>
-		<p>Suhaimi Saad KCS15007 </p>
-</body>
-</html>"""
+    return "Hello world!"
 
 
 if __name__ == "__main__":
