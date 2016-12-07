9a0b42e2fdd468bdbc842c3705166ce8d1872d13
fara1601 <fara_1601@yahoo.com>
2016-12-07 21:55:51 +0800

    delete image

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><b><center>Mini Project SCM <p>Marziah, Fara, Sarina, Suhaimi</center></b>
-            <p><img src="img/robot.jpg" width="600" height="200" />
-	</html>"
+    return "<html><b><center>Mini Project SCM <p>Marziah, Fara, Sarina, Suhaimi</center></b></html>"
 
 
 if __name__ == "__main__":
