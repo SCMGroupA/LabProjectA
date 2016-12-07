bf4104bcb9418b6f80a8c911ed91061770aecac5
fara1601 <fara_1601@yahoo.com>
2016-12-07 15:29:05 +0800

    insert html in codes

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+<% @app.route("/")%>
+<% def hello():
     return "Hello World, I love continuous delivery a lot and I enjoyed learn SCM!.. make changes..tambahan tambahan tambahan code"
+
+<% Marziah %>
+<% Fara %>
+<% Sarina %>
+<% Suhaimi %>
 
 if __name__ == "__main__":
     app.run()
+
+%>
