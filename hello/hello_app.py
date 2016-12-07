664af60e42e3f20119531b7d047b17a47c57e7b8
fara1601 <fara_1601@yahoo.com>
2016-12-07 15:37:56 +0800

    xxx

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, I love continuous delivery a lot and I enjoyed learn SCM!.. make changes..tambahan tambahan tambahan code"
+    return """"<html>
+<head></head>
+<body><p><b>Hello World!</b></p></body>
+</html>"""
+
 
 if __name__ == "__main__":
     app.run()
