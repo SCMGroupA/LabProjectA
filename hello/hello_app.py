52976849ff394c5aa2242f24dc1779f9da8bc445
Sarah <sarina.kptm@yahoo.com>
2016-12-09 16:00:26 +0800

    make some editing

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return " Hello World :), I love continuous delivery a lot and I enjoyed learn SCM!..Yes best ..edited by Marziah"
+    return " Hello World :), I love continuous delivery a lot and I enjoyed learn SCM!..Yes best ..edited by Sarina binti Abu Bakar KCS15004"
 
 if __name__ == "__main__//":
     app.run()
