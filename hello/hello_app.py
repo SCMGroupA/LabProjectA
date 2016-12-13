8cc6037a6b6df76b561c9ccdf2fbe61c9dfca18e
Sarah <sarina.kptm@yahoo.com>
2016-12-13 12:11:55 +0800

    delete text
    Delete text in file

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return " Hello World :), I love continuous delivery a lot and I enjoyed learn SCM!..Yes best ..Dont forget to study for your final exam..OK! edited by Sarina binti Abu Bakar KCS15004"
+    return " Hello World :), "
 
 if __name__ == "__main__//":
     app.run()
